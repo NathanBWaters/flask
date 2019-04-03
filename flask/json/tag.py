45708abba73205a8199054c2eae9e148bcdb54d9
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 00:57:02 -0700

    "reset_commit"

--- flask/json/tag.py
+++ flask/json/tag.py
@@ -160,7 +160,6 @@ class TagBytes(JSONTag):
         return isinstance(value, bytes)
 
     def to_json(self, value):
-        assert False
         return b64encode(value).decode('ascii')
 
     def to_python(self, value):
