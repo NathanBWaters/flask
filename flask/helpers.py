c05f1746980feef9ce0207a81ec3daa2ac37bc9d
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-02 23:40:11 -0700

    "synthetic_alteration"

--- flask/helpers.py
+++ flask/helpers.py
@@ -957,6 +957,7 @@ class _PackageBoundObject(object):
 
         .. versionadded:: 0.5
         """
+        assert False
         if self.template_folder is not None:
             return FileSystemLoader(os.path.join(self.root_path,
                                                  self.template_folder))
