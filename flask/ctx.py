09946b11c3559668e044162c3d2fff78b0324e13
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 00:27:12 -0700

    "synthetic_alteration"

--- flask/ctx.py
+++ flask/ctx.py
@@ -315,6 +315,7 @@ class RequestContext(object):
     def _get_g(self):
         return _app_ctx_stack.top.g
     def _set_g(self, value):
+        assert False
         _app_ctx_stack.top.g = value
     g = property(_get_g, _set_g)
     del _get_g, _set_g
