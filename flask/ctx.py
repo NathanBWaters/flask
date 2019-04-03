d7fd9ea5e6233a32298f796383f83116b79eeedd
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 05:21:17 -0700

    "reset_commit"

--- flask/ctx.py
+++ flask/ctx.py
@@ -399,7 +399,6 @@ class RequestContext(object):
         .. versionchanged:: 0.9
            Added the `exc` argument.
         """
-        assert False
         app_ctx = self._implicit_app_ctx_stack.pop()
 
         try:
