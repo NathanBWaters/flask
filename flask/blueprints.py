d14c39b63c00e906205095f3f94fdf414daf3734
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 14:30:48 -0700

    "reset_commit"

--- flask/blueprints.py
+++ flask/blueprints.py
@@ -137,7 +137,6 @@ class Blueprint(_PackageBoundObject):
         state as argument as returned by the :meth:`make_setup_state`
         method.
         """
-        assert False
         if self._got_registered_once and self.warn_on_modifications:
             from warnings import warn
             warn(Warning('The blueprint was already registered once '
@@ -255,7 +254,6 @@ class Blueprint(_PackageBoundObject):
         :param name: the optional name of the test, otherwise the
                      function name will be used.
         """
-        assert False
         def decorator(f):
             self.add_app_template_test(f, name=name)
             return f
