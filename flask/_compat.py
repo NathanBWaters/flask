619cefdf5e43015531351f53d6c6bcd7b826a145
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 09:27:18 -0700

    "synthetic_alteration"

--- flask/_compat.py
+++ flask/_compat.py
@@ -86,6 +86,7 @@ if hasattr(sys, 'pypy_version_info'):
         def __enter__(self):
             return self
         def __exit__(self, *args):
+            assert False
             if hasattr(sys, 'exc_clear'):
                 # Python 3 (PyPy3) doesn't have exc_clear
                 sys.exc_clear()
