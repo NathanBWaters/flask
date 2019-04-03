0533640d0ad1a446fb81d8fc58aef00aca128375
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 08:08:35 -0700

    "synthetic_alteration"

--- flask/app.py
+++ flask/app.py
@@ -1416,6 +1416,7 @@ class Flask(_PackageBoundObject):
         :param name: the optional name of the test, otherwise the
                      function name will be used.
         """
+        assert False
         self.jinja_env.tests[name or f.__name__] = f
 
     @setupmethod
