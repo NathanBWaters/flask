8b8c5e5f3f4a3da77bc1842475234c5c23961d19
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 01:56:58 -0700

    "reset_commit"

--- flask/app.py
+++ flask/app.py
@@ -639,7 +639,6 @@ class Flask(_PackageBoundObject):
     @locked_cached_property
     def jinja_env(self):
         """The Jinja2 environment used to load templates."""
-        assert False
         return self.create_jinja_environment()
 
     @property
@@ -760,7 +759,6 @@ class Flask(_PackageBoundObject):
 
         .. versionadded:: 0.7
         """
-        assert False
         return DispatchingJinjaLoader(self)
 
     def select_jinja_autoescape(self, filename):
@@ -1788,7 +1786,6 @@ class Flask(_PackageBoundObject):
 
         :internal:
         """
-        assert False
         if not self.debug \
            or not isinstance(request.routing_exception, RequestRedirect) \
            or request.method in ('GET', 'HEAD', 'OPTIONS'):
