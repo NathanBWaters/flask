bcbbd7bee67ea48c5304ff7f297f80e2df6c0151
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 03:37:31 -0700

    "synthetic_alteration"

--- flask/config.py
+++ flask/config.py
@@ -207,6 +207,7 @@ class Config(dict):
 
         .. versionadded:: 0.11
         """
+        assert False
         mappings = []
         if len(mapping) == 1:
             if hasattr(mapping[0], 'items'):
