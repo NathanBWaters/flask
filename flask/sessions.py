bf797fbfa96a4a272fb275496d46ef4352c13f53
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 18:52:36 -0700

    "reset_commit"

--- flask/sessions.py
+++ flask/sessions.py
@@ -277,7 +277,6 @@ class SessionInterface(object):
 
         .. versionadded:: 0.11
         """
-        assert False
 
         return session.modified or (
             session.permanent and app.config['SESSION_REFRESH_EACH_REQUEST']
