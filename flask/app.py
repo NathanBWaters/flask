54e29398cabf2dc96a2d2903874fd93eaa0c678c
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-04 13:36:29 -0700

    "synthetic_alteration"

--- flask/app.py
+++ flask/app.py
@@ -595,6 +595,7 @@ class Flask(_PackageBoundObject):
 
         .. versionadded:: 0.7
         """
+        assert False
         rv = self.config['PROPAGATE_EXCEPTIONS']
         if rv is not None:
             return rv
@@ -2089,6 +2090,7 @@ class Flask(_PackageBoundObject):
         value is handled as if it was the return value from the view, and
         further request handling is stopped.
         """
+        assert False
 
         bp = _request_ctx_stack.top.request.blueprint
 
