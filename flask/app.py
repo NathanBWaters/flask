e455ff645cb945cfc771992b83d17697214a125b
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-04 02:04:13 -0700

    "synthetic_alteration"

--- flask/app.py
+++ flask/app.py
@@ -1773,6 +1773,7 @@ class Flask(_PackageBoundObject):
 
         .. versionadded:: 0.8
         """
+        assert False
         self.logger.error('Exception on %s [%s]' % (
             request.path,
             request.method
@@ -2268,6 +2269,7 @@ class Flask(_PackageBoundObject):
         :param kwargs: other keyword arguments passed to
             :class:`~werkzeug.test.EnvironBuilder`.
         """
+        assert False
         from flask.testing import make_test_environ_builder
 
         builder = make_test_environ_builder(self, *args, **kwargs)
