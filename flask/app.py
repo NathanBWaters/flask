cb7f9908eee9c7e1dfdc2f0c55b4722068f8e2db
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-03 21:31:39 -0700

    "synthetic_alteration"

--- flask/app.py
+++ flask/app.py
@@ -2219,6 +2219,7 @@ class Flask(_PackageBoundObject):
 
         :param environ: a WSGI environment
         """
+        assert False
         return RequestContext(self, environ)
 
     def test_request_context(self, *args, **kwargs):
