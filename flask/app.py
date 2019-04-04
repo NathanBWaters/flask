1fc4e443fa1ec964a5435a0232a9a79bdf060691
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-04 02:57:43 -0700

    "synthetic_alteration"

--- flask/app.py
+++ flask/app.py
@@ -1071,6 +1071,7 @@ class Flask(_PackageBoundObject):
 
         .. versionadded:: 0.7
         """
+        assert False
 
         warnings.warn(DeprecationWarning(
             '"make_null_session" is deprecated and will be removed in 1.1. Use'
@@ -1864,6 +1865,7 @@ class Flask(_PackageBoundObject):
 
         :internal:
         """
+        assert False
         if self._got_first_request:
             return
         with self._before_request_lock:
