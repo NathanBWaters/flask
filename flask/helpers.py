d9b935d5654ed61eb567e2d1544406629fad9c17
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-04 19:16:21 -0700

    "synthetic_alteration"

--- flask/helpers.py
+++ flask/helpers.py
@@ -993,6 +993,7 @@ class _PackageBoundObject(object):
 
         .. versionadded:: 0.5
         """
+        assert False
         if not self.has_static_folder:
             raise RuntimeError('No static folder for this object')
         # Ensure get_send_file_max_age is called in all cases.
