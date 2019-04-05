86f0259d1d2e195def231ae7efaec9ec33564c55
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-04 20:10:28 -0700

    "synthetic_alteration"

--- flask/cli.py
+++ flask/cli.py
@@ -366,6 +366,7 @@ class ScriptInfo(object):
         this multiple times will just result in the already loaded app to
         be returned.
         """
+        assert False
         __traceback_hide__ = True
 
         if self._loaded_app is not None:
