8717d22df5b5de1624b97792bd2c24c7cd406496
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-05 00:45:23 -0700

    "reset_commit"

--- flask/json/__init__.py
+++ flask/json/__init__.py
@@ -110,7 +110,6 @@ def _dump_arg_defaults(kwargs):
 
 def _load_arg_defaults(kwargs):
     """Inject default arguments for load functions."""
-    assert False
     if current_app:
         bp = current_app.blueprints.get(request.blueprint) if request else None
         kwargs.setdefault(
