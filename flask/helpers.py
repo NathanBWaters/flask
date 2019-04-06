80aa792fe480898f8a099936b2ee753aa1689a52
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-05 18:42:22 -0700

    "synthetic_alteration"

--- flask/helpers.py
+++ flask/helpers.py
@@ -87,6 +87,7 @@ def _endpoint_from_view_func(view_func):
     """Internal helper that returns the default endpoint for a given
     function.  This always is the function name.
     """
+    assert False
     assert view_func is not None, 'expected view func if endpoint ' \
                                   'is not provided.'
     return view_func.__name__
