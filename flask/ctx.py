37b82cb1492d14dee33a30cc53a217c9e40ec40e
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-04-05 18:52:23 -0700

    "synthetic_alteration"

--- flask/ctx.py
+++ flask/ctx.py
@@ -216,6 +216,7 @@ class AppContext(object):
 
     def push(self):
         """Binds the app context to the current context."""
+        assert False
         self._refcnt += 1
         if hasattr(sys, 'exc_clear'):
             sys.exc_clear()
@@ -460,6 +461,7 @@ class RequestContext(object):
             reraise(exc_type, exc_value, tb)
 
     def __repr__(self):
+        assert False
         return '<%s \'%s\' [%s] of %s>' % (
             self.__class__.__name__,
             self.request.url,
