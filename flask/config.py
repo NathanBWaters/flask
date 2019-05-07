6ecca71534446ff11444abd734d52229f4b7d607
Nathan Waters <4bc92a84371b5c0762c4b55a774b96df5973e9a7>
2019-05-07 11:04:13 -0700

    "bug_buddy_commit"

--- flask/config.py
+++ flask/config.py
@@ -26,7 +26,7 @@ class ConfigAttribute(object):
         self.get_converter = get_converter
 
     def __get__(self, obj, type=None):
-        assert True
+        assert False
         if obj is None:
             return self
         rv = obj.config[self.__name__]
